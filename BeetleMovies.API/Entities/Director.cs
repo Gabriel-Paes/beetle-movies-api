bdb7ef6bc71099f52b6dd758f0bc8b79f7b0b275
Gabriel-Paes <paessouzagabriel@gmail.com>
2024-07-08 22:12:42 -0300

    Adding Entities Director and Movie

---
+++ BeetleMovies.API/Entities/Director.cs
@@ -0,0 +1,16 @@
+﻿namespace BeetleMovies.API;
+
+public class Director
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public ICollection<Movie> Movies { get; set; } = [];
+
+    public Director() { }
+
+    public Director(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
---
+++ BeetleMovies.API/Entities/Movie.cs
@@ -0,0 +1,20 @@
+﻿namespace BeetleMovies.API;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public int Year { get; set; }
+    public int Rating { get; set; }
+    public ICollection<Director> Directors { get; set; } = [];
+
+    public Movie() { }
+
+    public Movie(int id, string title, int year, int rating)
+    {
+        Id = id;
+        Title = title;
+        Year = year;
+        Rating = rating;
+    }
+}
