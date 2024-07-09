d70956baaf930ae2c70d124d41011f1c905da75c
Gabriel-Paes <paessouzagabriel@gmail.com>
2024-07-08 23:25:42 -0300

    Adding EF and DBContexts

---
+++ BeetleMovies.API/DBContexts/BeetleMoviesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BeetleMovies.API;
+
+public class BeetleMoviesContext(DbContextOptions<BeetleMoviesContext> options) : DbContext(options)
+{
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<Director> Directors { get; set; }
+
+    override protected void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- BeetleMovies.API/Program.cs
+++ BeetleMovies.API/Program.cs
@@ -1,4 +1,12 @@
+using BeetleMovies.API;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BeetleMoviesContext>(
+    o => o.UseSqlite(builder.Configuration["ConnectionStrings:BeetleMoviesStr"])
+);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
